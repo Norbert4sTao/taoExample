6714df148aa367f113a82abab9e61262563b80c8
Norbert4sTao <norbertas@taotesting.com>
2018-10-25 15:48:13 +0200

    basic

---
+++ controller/Greet.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace oat\taoExample\controller;
+
+class Greeter extends \tao_actions_commonModule {
+    /**
+     * initialize the services
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function helloFromTemplate() {
+        $this->setData('name', 'tao');
+        $this->setView('Greeter/myName.tpl');
+    }
+
+}
+
+?>
